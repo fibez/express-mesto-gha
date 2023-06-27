62cdb5173e65cea59705fa3443333407e5385617
Artem Eliseev <aeliseevd@yandex.ru>
2023-06-27 16:59:06 +0400

    add card data validation via JOI

--- middlewares/celebrateValidation.js
+++ middlewares/celebrateValidation.js
@@ -38,10 +38,25 @@ const userAvatarUrlValidator = celebrate({
   }),
 });
 
+const cardDataValidator = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(urlPattern),
+  }),
+});
+
+const cardIdValidator = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+});
+
 module.exports = {
   userSchemaSignupValidator,
   userSchemaSigninValidator,
   userIdValidator,
   userNameAboutValidator,
   userAvatarUrlValidator,
+  cardDataValidator,
+  cardIdValidator,
 };
--- routes/cards.js
+++ routes/cards.js
@@ -8,11 +8,15 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 const { auth } = require('../middlewares/auth');
+const {
+  cardDataValidator,
+  cardIdValidator,
+} = require('../middlewares/celebrateValidation');
 
 router.get('/cards', auth, getAllCards);
-router.post('/cards', auth, createCard);
-router.delete('/cards/:cardId', auth, deleteCard);
-router.put('/cards/:cardId/likes', auth, likeCard);
-router.delete('/cards/:cardId/likes', auth, dislikeCard);
+router.post('/cards', auth, cardDataValidator, createCard);
+router.delete('/cards/:cardId', auth, cardIdValidator, deleteCard);
+router.put('/cards/:cardId/likes', auth, cardIdValidator, likeCard);
+router.delete('/cards/:cardId/likes', auth, cardIdValidator, dislikeCard);
 
 module.exports = router;
